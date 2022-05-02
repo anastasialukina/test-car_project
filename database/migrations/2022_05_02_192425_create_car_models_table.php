97eb419bc5085e0f363cc5a53a93a94e09ba1eb4
Anastasia <anastasia.lukina1602@icloud.com>
2022-05-03 00:48:16 +0400

    Add bootstrap Add database Add entities: car model, car brand and car Add migrations Add settings & .env configs

---
+++ database/migrations/2022_05_02_192425_create_car_models_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->id()->comment('Identifier');
+
+            $table->bigInteger('brand_id')->unsigned()->comment('Brand ID');
+            $table->foreign('brand_id')->references('id')->on('car_brands')->restrictOnDelete();
+
+            $table->string('name', 100)->index()->comment('Name of the car model');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_models');
+    }
+};
---
+++ database/migrations/2022_05_02_192440_create_cars_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id()->comment('Identifier');
+
+            $table->bigInteger('model_id')->unsigned()->comment('Model ID');
+            $table->foreign('model_id')->references('id')->on('car_models')->restrictOnDelete();
+
+            $table->string('number', 15)->index()->comment('Car number');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
